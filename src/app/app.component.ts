b3f75a30ef24864588cf9b9e5f46c3afe8617c76
Fernanda Yonezawa Sato <100849359+fehsato@users.noreply.github.com>
2023-05-09 16:49:24 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,55 @@ import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
-import { AppRoutingModule } from './app-routing.module';
+
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  readonly apiURL ['https://pokeapi.co/api/v2/pokemon/1'] : string;
 })
 export class AppComponent {
   title = 'pokedex';
-  constructor(private http : HttpClient) {}
+  apiURL: string = 'https://pokeapi.co/api/v2';
+  lista: any = [];
+  pokemon: any;
+  pagina = 0
+  proximo: string = `${this.apiURL}/pokemon?offset=${this.pagina * 20}`;
   
+  constructor(private http: HttpClient) { }
+
+  listarTudo() {
+    this.http.get(`${this.apiURL}/pokemon?offset=${this.pagina * 20}`).subscribe((resultado: any) => {
+      this.lista = resultado.results;
+      
+      console.log(resultado);
+      
+      for (let index = 0; index < this.lista.length; index++) {
+        this.http.get(this.lista[index].url).subscribe((imagem: any) => {
+          this.lista[index].imagem = imagem.sprites.front_default
+          
+        });
+      }
+
+    });
+  }
+
+  proximapagina(){
+    this.pagina++
+    this.listarTudo();
+  }
+  paginaanterior(){
+    this.pagina--
+    this.listarTudo();
+  }
+  pegarpokemon(nomepokemon:string){
+    this.http.get(`${this.apiURL}/pokemon/${nomepokemon}`).subscribe((name: any) => {
+      console.log(name);
+      this.pokemon=name;
+    });
+  }
 }
 
-constructor() {
-  this
-}
